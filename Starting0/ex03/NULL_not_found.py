deb8cbb34f05d6685c05c99b3fadf1eeefeb7902
kosnakam <kosnakam@f2r8s3.clusters.42paris.fr>
2025-10-22 18:17:55 +0200

    func: implemented ex03

---
+++ Starting0/ex03/NULL_not_found.py
@@ -0,0 +1,16 @@
+def NULL_not_found(object: any) -> int:
+
+	if (object == None):
+		print("Nothing", ": ", object, " ", type(object), sep='')
+	elif (object != object):
+		print("Cheese", ": ", object, " ", type(object), sep='')
+	elif (object == 0 and type(object) == int):
+		print("Zero", ": ", object, " ", type(object), sep='')
+	elif (object == '' and type(object) == str):
+		print("Empty", ": ", object, type(object), sep='')
+	elif (object == False and type(object) == bool):
+		print("Fake", ": ", object, " ", type(object), sep='')
+	else:
+		print("Type not found")
+		return 1
+	return 0
