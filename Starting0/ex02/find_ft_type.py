00a5c6f08ead6942688deeafc86721b12130070c
kosnakam <kosnakam@f2r8s3.clusters.42paris.fr>
2025-10-22 18:17:19 +0200

    func: implemented ex02

---
+++ Starting0/ex02/find_ft_type.py
@@ -0,0 +1,18 @@
+def all_thing_is_obj(object: any) -> int:
+
+	ft_dict = {
+		list: "List",
+		tuple: "Tuple",
+		set: "Set",
+		dict: "Dict",
+		str: f"{object}" " is in the kitchen"
+	}
+
+	ft_type = type(object)
+
+	if ft_type in ft_dict:
+		print(ft_dict[ft_type], ":", ft_type)
+	else:
+		print("Type not found")
+	
+	return 42
