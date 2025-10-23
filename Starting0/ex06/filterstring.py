832c4c52e22cb4d749bc7343c5f73d03650d3c75
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-23 12:30:54 +0200

    func: implemented main, ft_filter

--- Starting0/ex06/filterstring.py
+++ Starting0/ex06/filterstring.py
@@ -1,17 +1,19 @@
 import sys
 from ft_filter import ft_filter
 
+
 def main():
+    """Main function to filter strings based on length."""
     try:
-        assert len(sys.argv) == 3, "AssertionError: the arguments are bad"
+        assert len(sys.argv) == 3
+        assert sys.argv[2].isdigit()
         input_string = sys.argv[1].split()
         filtered = ft_filter(lambda x: len(x) > int(sys.argv[2]), input_string)
-        print(filtered)
         print("[", ", ".join(filtered), "]", sep="")
 
-    except AssertionError as e:
-        print(f"AssertionError: {e}")
-        return
+    except AssertionError:
+        print("AssertionError: the arguments are bad")
+    return
 
 
 if __name__ == "__main__":
--- Starting0/ex06/ft_filter.py
+++ Starting0/ex06/ft_filter.py
@@ -1,2 +1,6 @@
 def ft_filter(func, iterable):
-    return (item for item in iterable if func(item))
+    """filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
+    return ("\'" + item + "\'" for item in iterable if func(item))
