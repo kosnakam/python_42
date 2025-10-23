0dbb95bb477d25705c3eb820a285f9643f5f4f61
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-23 11:59:01 +0200

    doc: add docs

--- Starting0/ex05/building.py
+++ Starting0/ex05/building.py
@@ -1,66 +1,32 @@
 import sys
+import string
 
 
 def main():
+    """
+    Main function to analyze text input from command line arguments.
+    """
     try:
         assert len(sys.argv) == 2, "Please provide exactly one argument."
-        print("The text contains", count_char(sys.argv[1]), "characters:")
-        print(count_upper(sys.argv[1]), "upper letters")
-        print(count_lower(sys.argv[1]), "lower letters")
-        print(count_punctuation(sys.argv[1]), "punctuation marks")
-        print(count_spaces(sys.argv[1]), "spaces")
-        print(count_digits(sys.argv[1]), "digits")
+        print("The text contains", len(sys.argv[1]), "characters:")
+        print(
+            sum(1 for char in sys.argv[1] if char.isupper()), "upper letters"
+            )
+        print(
+            sum(1 for char in sys.argv[1] if char.islower()), "lower letters"
+            )
+        print(
+            sum(
+                1 for char in sys.argv[1] if char in string.punctuation
+                ), "punctuation marks"
+            )
+        print(sum(1 for char in sys.argv[1] if char.isspace()), "spaces")
+        print(sum(1 for char in sys.argv[1] if char.isdigit()), "digits")
     except AssertionError as e:
         print(f"Error: {e}")
-        return
-
-
-def count_char(s: str) -> int:
-    try:
-        return len(s)
     except Exception as e:
         print(f"Error: {e}")
-        return 0
-
-
-def count_upper(s: str) -> int:
-    try:
-        return sum(1 for char in s if char.isupper())
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
-
-
-def count_lower(s: str) -> int:
-    try:
-        return sum(1 for char in s if char.islower())
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
-
-
-def count_punctuation(s: str) -> int:
-    try:
-        return sum(1 for char in s if char in '.,;:!?\'"()-')
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
-
-
-def count_spaces(s: str) -> int:
-    try:
-        return sum(1 for char in s if char.isspace())
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
-
-
-def count_digits(s: str) -> int:
-    try:
-        return sum(1 for char in s if char.isdigit())
-    except Exception as e:
-        print(f"Error: {e}")
-        return 0
+    return
 
 
 if __name__ == "__main__":
