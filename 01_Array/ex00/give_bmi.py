33e1f702b2b9532dc1d0beaa78235ca6e558fbca
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-24 14:05:04 +0200

    func: implemented give_bmi, apply_limit

--- 01_Array/ex00/give_bmi.py
+++ 01_Array/ex00/give_bmi.py
@@ -7,15 +7,25 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
         assert all(isinstance(element, (int, float)) for element in height), "Height lists of arguments has to have int or float!"
         assert all(isinstance(element, (int, float)) for element in weight), "Weight lists of arguments has to have int or float!"
         
-        ret = None
+        ret = []
         for h, w in zip(height, weight):
-            ret.append(w/)
-
-
+            ret.append(w/pow(h, 2))
 
     except AssertionError as e:
         print(f"Error(give_bmi): {e}")
-    return
+        return None
+    return ret
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    return
+    try:
+        assert type(limit) == int, "The second argument must be of type int!"
+        assert all(isinstance(element, (float, int)) for element in bmi), "Height lists of arguments has to have int or float!"
+
+        ret = []
+        for element in bmi:
+            ret.append(True if element > limit else False)
+
+    except AssertionError as e:
+        print(f"Error(apply_limit): {e}")
+        return None
+    return ret
