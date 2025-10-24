1183d7a4d497aaf3d3959b4f2c5ff4065c3482f3
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-24 15:11:02 +0200

    norminette

--- 01_Array/ex00/give_bmi.py
+++ 01_Array/ex00/give_bmi.py
@@ -1,12 +1,18 @@
-import numpy
-
-
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(
+        height: list[int | float], weight: list[int | float]
+        ) -> list[int | float]:
+    """This function return the bmi list."""
     try:
-        assert len(height) == len(weight), "Lists of arguments must be the same size!"
-        assert all(isinstance(element, (int, float)) for element in height), "Height lists of arguments has to have int or float!"
-        assert all(isinstance(element, (int, float)) for element in weight), "Weight lists of arguments has to have int or float!"
-        
+        assert len(height) == len(
+            weight
+            ), "Lists of arguments must be the same size!"
+        assert all(
+            isinstance(element, (int, float)) for element in height
+            ), "Height lists of arguments has to have int or float!"
+        assert all(
+            isinstance(element, (int, float)) for element in weight
+            ), "Weight lists of arguments has to have int or float!"
+
         ret = []
         for h, w in zip(height, weight):
             ret.append(w/pow(h, 2))
@@ -16,10 +22,14 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
         return None
     return ret
 
+
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """This function compare bmi and oranges."""
     try:
-        assert type(limit) == int, "The second argument must be of type int!"
-        assert all(isinstance(element, (float, int)) for element in bmi), "Height lists of arguments has to have int or float!"
+        assert type(limit) is int, "The second argument must be of type int!"
+        assert all(
+            isinstance(element, (float, int)) for element in bmi
+            ), "Height lists of arguments has to have int or float!"
 
         ret = []
         for element in bmi:
@@ -29,3 +39,7 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
         print(f"Error(apply_limit): {e}")
         return None
     return ret
+
+
+if __name__ == "__main__":
+    print("This is a module that dose not contain a main function.")
