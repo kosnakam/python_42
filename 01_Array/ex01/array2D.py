fc8b603c8d91d07cb7fb7a192c13886cad5dc185
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-27 12:00:26 +0100

    func: implemented slice_me

---
+++ 01_Array/ex01/array2D.py
@@ -0,0 +1,15 @@
+def slice_me(family: list, start: int, end: int) -> list:
+    """slice(stop)
+slice(start, stop[, step])
+
+Create a slice object.  This is used for extended slicing (e.g. a[0:10:2])."""
+    try:
+        assert type(family) is list and type(start) is int and type(end) is int, "These arguments are not accepted."
+        assert len(family) > end and len(family) >= start, "The lists are not the same size."
+
+        ret = family[start:end]
+
+    except AssertionError as e:
+        print(f"Error(slice_me): {e}")
+
+    return ret
