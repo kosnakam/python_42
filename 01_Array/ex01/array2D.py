04b1abc4b6e6aac1f963be39ce8437fdeda76a57
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-27 12:07:55 +0100

    norminette

--- 01_Array/ex01/array2D.py
+++ 01_Array/ex01/array2D.py
@@ -1,11 +1,11 @@
-def slice_me(family: list, start: int, end: int) -> list:
-    """slice(stop)
-slice(start, stop[, step])
 
-Create a slice object.  This is used for extended slicing (e.g. a[0:10:2])."""
+def slice_me(family: list, start: int, end: int) -> list:
+    """This function is like a slice."""
     try:
-        assert type(family) is list and type(start) is int and type(end) is int, "These arguments are not accepted."
-        assert len(family) > end and len(family) >= start, "The lists are not the same size."
+        assert type(family) is list and type(start) is int and type(
+            end) is int, "These arguments are not accepted."
+        assert len(family) > end and len(
+            family) >= start, "The lists are not the same size."
 
         ret = family[start:end]
 
