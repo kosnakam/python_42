0ef570e2e7e0767e4bb89fb941cfede410f86c48
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-27 14:25:14 +0100

    func: implemented ft_load

---
+++ 01_Array/ex02/load_image.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str) -> list[list[int]]:
+    """This function loads path and analyzes the shape."""
+    try:
+        assert type(path) is not None, "This argument is not accepted."
+
+        im = Image.open(path)
+        ret = np.array(im)
+        print(f"The shape of image is: {ret.shape}")
+
+    except AssertionError as e:
+        print(f"Error(ft_load): {e}")
+        return None
+    return ret
