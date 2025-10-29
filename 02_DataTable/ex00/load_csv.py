a157ef720a744c93b0a7445250c07fa6644bdd5a
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-10-29 14:47:28 +0100

    norminette

--- 02_DataTable/ex00/load_csv.py
+++ 02_DataTable/ex00/load_csv.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def load(path: str) -> pd.core.frame.DataFrame:
+    """Reads the specified file and returns it."""
     try:
         assert type(path) is str, "This argument is not accepted."
 
@@ -9,6 +10,7 @@ def load(path: str) -> pd.core.frame.DataFrame:
         print(f"Loading dataset of dimensions {df.shape}")
         return df
 
-    except (AssertionError, FileNotFoundError, pd.errors.EmptyDataError, pd.errors.ParserError) as e:
+    except (AssertionError, FileNotFoundError,
+            pd.errors.EmptyDataError, pd.errors.ParserError) as e:
         print(f"Error(load): {e}")
         return None
