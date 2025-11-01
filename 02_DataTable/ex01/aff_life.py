19e7bb81e6f7a484a14d75e32722e74fd55239a7
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-01 16:43:35 +0100

    func: implemented main

--- 02_DataTable/ex01/aff_life.py
+++ 02_DataTable/ex01/aff_life.py
@@ -1,10 +1,27 @@
 from load_csv import load
+import matplotlib.pyplot as plt
 
 
 def main():
-    df = load("life_expectancy_years.csv")
-    print(df)
-    return
+    try:
+    
+        df = load("life_expectancy_years.csv")
+        country = "Japan"
+        data = df[df['country'] == country]
+        years = [int(y) for y in data.columns[1:]]
+        values = data.iloc[0, 1:].astype(float)
+        xticks = list(range(min(years), max(years), 40))
+
+        plt.figure(figsize=(8,6))
+        plt.plot(years, values, label=country)
+        plt.xticks(xticks)
+        plt.title(f"{country} Life expectancy Projections")
+        plt.xlabel("Year")
+        plt.ylabel("Life expectancy")
+        plt.show()
+    
+    except Exception as e:
+        print(f"Error(main): {e}")
 
 
 if __name__ == "__main__":
