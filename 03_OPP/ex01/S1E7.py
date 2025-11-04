94ee77787f3a692cfee9023e0dd3ed037f728ba3
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-04 15:47:35 +0100

    norminette

--- 03_OPP/ex01/S1E7.py
+++ 03_OPP/ex01/S1E7.py
@@ -4,11 +4,17 @@ from S1E9 import Character
 class Baratheon(Character):
     """Representing the Baratheon family."""
     def __init__(self, first_name, is_alive=True):
+        """Your docstring for Constructor"""
         super().__init__(first_name, is_alive)
 
 
 class Lannister(Character):
-    def __init__(self, first_name, is_alive=True, family_name="Lannister", eyes="blue", hairs="light"):
+    """Representing the Lannister family."""
+    def __init__(
+            self, first_name, is_alive=True,
+            family_name="Lannister", eyes="blue", hairs="light"
+            ):
+        """Your docstring for Constructor"""
         super().__init__(first_name, is_alive)
         self.family_name = family_name
         self.eyes = eyes
@@ -16,5 +22,5 @@ class Lannister(Character):
 
     @classmethod
     def create_lannister(cls, first_name, is_alive=True):
+        """Your docstring for ClassMethod"""
         return cls(first_name, is_alive)
-        
