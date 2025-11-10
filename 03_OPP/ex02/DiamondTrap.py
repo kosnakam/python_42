d4cbec72f2c4ccc150c83f77337f487a307145b4
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-10 14:39:18 +0100

    func: implemented king class

---
+++ 03_OPP/ex02/DiamondTrap.py
@@ -0,0 +1,22 @@
+from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    """Your docstring for Class"""
+    def __init__(self, first_name, is_alive=True, family_name="Baratheon", eyes="brouwn", hairs="dark"):
+        """Your docstring for Constructor"""
+        super().__init__(first_name, is_alive, family_name, eyes, hairs)
+    
+    def set_eyes(self, arg : str) -> None:
+        """This function sets self.eyes = arg"""
+        self.eyes = arg
+    
+    def set_hairs(self, arg: str) -> None:
+        """This function sets self.eyes = arg"""
+        self.hairs = arg
+    
+    def get_eyes(self) -> None:
+        return self.eyes
+    
+    def get_hairs(self) -> None:
+        return self.hairs
---
+++ 03_OPP/ex02/S1E9.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Your docstring for Class"""
+    @abstractmethod
+    def __init__(self, first_name, is_alive):
+        """Your docstring for Constructor"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    def __str__(self):
+        """
+        To control how objects are represented as strings more user-friendly
+        """
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """
+        To control how objects are represented as strings more official
+        """
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def die(self):
+        """Your docstring for Method"""
+        self.is_alive = False
+
+
+class Stark(Character):
+    """Your docstring for Class"""
+    def __init__(self, first_name, is_alive=True):
+        """Your docstring for Constructor"""
+        super().__init__(first_name, is_alive)
