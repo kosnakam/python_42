de226c544d257afebbbfefdc29c57cc8ce9fa271
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-10 15:28:15 +0100

    refa: add __doc__

--- 03_OPP/ex03/ft_calculator.py
+++ 03_OPP/ex03/ft_calculator.py
@@ -1,19 +1,25 @@
 class calculator:
+    """A class implementing arithmetic operations"""
     def __init__(self, arg: list[float]):
+        """Constructor of this class"""
         self.lst = arg
 
     def __add__(self, object) -> None:
+        """Function of addition"""
         self.lst = [element + object for element in self.lst]
         print(self.lst)
 
     def __mul__(self, object) -> None:
+        """Function of Multiplication"""
         self.lst = [element * object for element in self.lst]
         print(self.lst)
 
     def __sub__(self, object) -> None:
+        """Function of Subtraction"""
         self.lst = [element - object for element in self.lst]
         print(self.lst)
 
     def __truediv__(self, object) -> None:
+        """Function of Division"""
         self.lst = [element / object for element in self.lst]
         print(self.lst)
