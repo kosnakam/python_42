2c31bb3d6668e5258412523a5940205b4e88ab07
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-19 14:06:18 +0100

    func: change count to nonlocal

--- 04_Dod/ex01/in_out.py
+++ 04_Dod/ex01/in_out.py
@@ -1,16 +1,19 @@
 
-
 def square(x: int | float) -> int | float:
     return x ** 2
+
 
 def pow(x: int | float) -> int | float:
     return x ** x
 
+
 def outer(x: int | float, function) -> object:
     count = 0
+
     def inner() -> float:
-        nonlocal x
+        nonlocal x, count
+        count += 1
         x = function(x)
         return x
-    count += 1
+
     return inner
