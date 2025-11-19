6554be4d7aff11fe0638781b6a5056ec633a46d8
kosnakam <mnbvcxzmnbvcx@outlook.jp>
2025-11-19 14:09:18 +0100

    norminette

--- 04_Dod/ex01/in_out.py
+++ 04_Dod/ex01/in_out.py
@@ -1,16 +1,20 @@
 
 def square(x: int | float) -> int | float:
+    """A function calculating the square."""
     return x ** 2
 
 
 def pow(x: int | float) -> int | float:
+    """A function calculating the pow."""
     return x ** x
 
 
 def outer(x: int | float, function) -> object:
+    """Outer funciton."""
     count = 0
 
     def inner() -> float:
+        """Inner function."""
         nonlocal x, count
         count += 1
         x = function(x)
